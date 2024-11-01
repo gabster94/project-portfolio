d3240382605201e36af6f3e00997b7e405619b91
gabster94 <iofe.gabriella@gmail.com>
2024-11-01 17:07:36 +0100

    created folders and files and json files

---
+++ my-portfolio/src/components/CardButton.jsx
@@ -0,0 +1,28 @@
+export const Button = ({
+  icon,
+  label,
+  link,
+  className,
+  onClick,
+  ariaLabel,
+}) => {
+  const handleClick = () => {
+    window.open(link, "_blank");
+  };
+
+  return (
+    <button
+      className={`button ${className || ""}`}
+      onClick={onClick || handleClick}
+      aria-label={ariaLabel || label} // Allow custom aria-label
+    >
+      <img
+        src={icon}
+        alt=""
+        className="icon"
+        aria-hidden="true" // Hide the icon from assistive technologies
+      />
+      <span className="label">{label}</span>
+    </button>
+  );
+};
